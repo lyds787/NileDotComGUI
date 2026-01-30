d85ebeeb23f5067c13b2e6fd7fb05de47d5ef98c
lyds787 <152084402+lyds787@users.noreply.github.com>
2026-01-29 21:45:16 -0500

    Update NileDotComGUI.java
    Starting

--- NileDotComGUI.java
+++ NileDotComGUI.java
@@ -1,1 +1,11 @@
+import javax.swing.*;
+
+public class NileDotComGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Nile.com");
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
 
